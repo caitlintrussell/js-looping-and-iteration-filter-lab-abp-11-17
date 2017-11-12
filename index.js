a84b12f6fdbebfb4bbb550c725b5cdf30b4bb891
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 22:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 
 
-const findMatching(drivers, string) {
-  let driver = drivers.filter(function(string){
-    return string;
+function findMatch(drivers, name){
+  drivers.filter(function(driver){
+    if (driver.toLowerCase() === name.toLowerCase()){
+      return driver;
+    }
+      
+  )  
   }
 }
-
-
-// var longWords = words.filter(function(word){
-//   return word.length > 6;
-// });
-//
-//   name: 'Bobby',
-//   hometown: 'Pittsburgh' },
-// {
-//   name: 'Sammy',
-//   hometown: 'New York' } ,
-// {
-//   name: 'Sally',
-//   hometown: 'Cleveland' },
-// {
-//   name: 'Annette',
-//   hometown: 'Los Angeles' },
-// {
-//   name: 'Bobby',
-//   hometown: 'Tampa Bay' }
-// ];
