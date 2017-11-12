b40009110299f866d543856e1bb2197fc1663729
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 23:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,6 @@ function fuzzyMatch(array, letters){
   let lengthOfName = letters.length;
   return array.filter(function(driverName){
     return driverName.slice(0, lengthOfName) === letters
-  })
-return fuzzyMatchingDriver;
-  }
-  // 
-  // function fuzzyMatch (list, partialName) {
-  //   let lengthOfName = partialName.length;
-  //   return list.filter(function (driverName) {
-  //     return driverName.slice(0, lengthOfName) === partialName;
-  //   });
+  })}
+
+  
