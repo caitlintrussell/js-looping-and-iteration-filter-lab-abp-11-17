a46d95cf1b97111b5209840b245a61cc3b4d37a1
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 22:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name){
+   drivers.filter(function(name){
+  return drivers = name;
+});
+}
